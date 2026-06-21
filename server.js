cf3d2092c3859b8185987d99cdc6a8a01b0cc748
Jeffrey <Jefferymsd101@gmail.com>
2026-06-21 15:29:17 +0530

    Add register route validation

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
+const {email,password} = req.body
+
+app.use(express.json());
+
 app.get("/",(req,res)=>{
     res.send("Hello Jeffrey")
 })
+
+app.post("/register",(req,res)=>{
+   const {email,password} = req.body 
+    if(email===""||password === ""){
+        return res.send("Please fill all fields")
+    }
+    console.log("User can register")
+    res.send("Register route working");
+})
+
 app.listen(3000,()=>{
     console.log("Server is running")
 })
