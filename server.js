0087ab52b2b79fd8152897ec05c34970d6c16699
Jeffrey <Jefferymsd101@gmail.com>
2026-06-22 21:01:38 +0530

    Add login route with password verification

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("./model/user");
+const bcrypt = require("bcryptjs")
 const app = express();
 
 app.use(express.json());
@@ -10,6 +11,7 @@ app.get("/",(req,res)=>{
 
 app.post("/register", async (req,res)=>{
 
+  try{
     const {email,password} = req.body;
 
     if(email===""||password === ""){
@@ -20,23 +22,57 @@ app.post("/register", async (req,res)=>{
 
     if (existingUser){
         return res.send("Email already exists")
-    }
+    };
+
+    const hashedPassword = await bcrypt.hash(password,10)
 
     const user = {
         email,
-        password
+        password: hashedPassword
     }
     console.log(user)
 
-    try{
-     await User.create(user)
-    }
-    catch{
+    await User.create(user)
+    res.send("User registered successfully")
+   }
+
+  catch(error){
+      console.log(error)
       res.send("Something went wrong")  
     }
-
-    res.send("Register route working");
 })
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (email === "" || password === "") {
+        return res.send("Please fill all fields");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return res.send("User not found");
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+        password,
+        user.password
+    );
+
+    if (!isPasswordCorrect) {
+        return res.send("Wrong password");
+    }
+
+    res.send("Login successful");
+   }
+
+ catch (error) {
+        console.log(error);
+        res.send("Something went wrong");
+    }
+});
 
 app.listen(3000,()=>{
     console.log("Server is running")
