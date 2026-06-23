57e3d9182cd56e48bf4aa99f7c6eadd2d6458b29
Jeffrey <Jefferymsd101@gmail.com>
2026-06-24 00:33:52 +0530

    Connect Tiny Auth app to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const express = require("express");
-const User = require("./model/user");
+const mongoose = require("mongoose")
 const bcrypt = require("bcryptjs")
 const app = express();
+const jwt = require("jsonwebtoken")
+
+const User = require("./models/User");
 
 app.use(express.json());
 
@@ -12,29 +19,29 @@ app.get("/",(req,res)=>{
 app.post("/register", async (req,res)=>{
 
   try{
-    const {email,password} = req.body;
+        const {email,password} = req.body;
 
-    if(email===""||password === ""){
-        return res.send("Please fill all fields")
+        if(email===""||password === ""){
+            return res.send("Please fill all fields")
+        }
+
+        const existingUser = await User.findOne({email});
+
+        if (existingUser){
+            return res.send("Email already exists")
+        };
+
+        const hashedPassword = await bcrypt.hash(password,10)
+
+        const user = {
+            email,
+            password: hashedPassword
+        }
+        console.log(user)
+
+        await User.create(user)
+        res.send("User registered successfully")
     }
-
-    const existingUser = await User.findOne({email});
-
-    if (existingUser){
-        return res.send("Email already exists")
-    };
-
-    const hashedPassword = await bcrypt.hash(password,10)
-
-    const user = {
-        email,
-        password: hashedPassword
-    }
-    console.log(user)
-
-    await User.create(user)
-    res.send("User registered successfully")
-   }
 
   catch(error){
       console.log(error)
@@ -65,15 +72,24 @@ app.post("/login", async (req, res) => {
         return res.send("Wrong password");
     }
 
-    res.send("Login successful");
-   }
+    const token = jwt.sign({ id: user._id }, "secretkey");
+    res.send(token);
+   } 
 
- catch (error) {
-        console.log(error);
-        res.send("Something went wrong");
+  catch (error) {
+    console.log(error);
+    res.send("Something went wrong");
     }
 });
 
-app.listen(3000,()=>{
-    console.log("Server is running")
-})
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(3000, () => {
+      console.log("Server is running");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+});
