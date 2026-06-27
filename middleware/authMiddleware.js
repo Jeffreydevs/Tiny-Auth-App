ba3ab0ba323f88160639794adf870c83f172b480
Jeffrey <Jefferymsd101@gmail.com>
2026-06-27 23:01:37 +0530

    Improve JWT middleware with Bearer token support

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
- const token = req.headers.authorization;
+ const authHeader = req.headers.authorization;
+ const token = authHeader.split(" ")[1]
  if (!token) {
  return res.send("Access denied");
  }
